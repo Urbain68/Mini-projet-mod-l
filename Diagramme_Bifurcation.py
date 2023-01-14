8e30d3f438b2f012acd8541a7f4d05b9f9024381
The-Sch-O <118555410+The-Sch-O@users.noreply.github.com>
2023-01-14 16:00:43 +0100

    A peu près fini le chapitre 3
    J'ai plus ou moins fini le chapitre sur les simus. J'ai rajouté une petite phrase pour tenter d'expliquer les différences entre calcul théorique et simu.

--- Diagramme_Bifurcation.py
+++ Diagramme_Bifurcation.py
@@ -40,7 +40,7 @@ ax1.set_title("Analyse pour les proies (x_n)")
 ax2.set_title("Analyse pour les prédateurs (y_n)")
 plt.xlim(1, 3)
 fig.suptitle("Diagramme de bifurcation")
-plt.show()
+# plt.show()
 
 
 
@@ -57,30 +57,48 @@ nu_lim = 0.04
 # Calcul des points d'équilibre pour les paramètres donnés :
 y_star = (sigma_lim*alpha_lim - rho_lim*gamma_lim)/(beta_lim*sigma_lim + nu_lim*gamma_lim)
 x_star = (alpha_lim - beta_lim*y_star)/gamma_lim
-print("point d'equilibre :")
-print(x_star, y_star)
+print("Point d'équilibre :")
+print("x*=",x_star,", y*=", y_star)
 
 # Coefficient de la jacobienne : J(x*, y*) = [[a1, a2], [a3, a4]]
 a1 = 1 + alpha_lim - beta_lim*y_star - 2*gamma_lim*x_star
 a2 = - beta_lim*x_star
 a3 = sigma_lim*y_star
 a4 = 1 - rho_lim + sigma_lim*x_star + 2*nu_lim*y_star
-print("coeffs matrice :")
-print(a1, a2, a3, a4)
+print("Coefficients de la matrice jacobienne au point d'équilibre :")
+print("J(x*, y*) =")
+print("[ [", a1, "  ", a2, "]\n","  [", a3, "  ", a4, "] ]")
 
 # Coefficients du polynôme caractéristique de la jacobienne : chi(X) = X^2 + b1*X + b2
 b1 = - (a1 + a4)
 b2 = a1*a4 - a2*a3
-print("coeffs polynome :")
+print("Polynôme caractéristique de la jacobienne :")
 print(f"X^2 + {b1}*X + {b2}")
 
 # Critère de Jury pour des polynômes d'ordre 2 :
 cond1 = b2 + b1 + 1
 cond2 = b2 - b1 + 1
 cond3 = abs(b2) - 1
-print("critère de Jury :")
-print(cond1, cond2, cond3)
+print("Valeurs des conditions du critère de Jury pour des polynômes de degré 2 :")
+print("Valeur de la condition 1 :",cond1)
+if cond1 > 0:
+    print("Condition vérifiée")
+else:
+    print("Condition NON vérifiée")
+
+print("Valeur de la condition 2 :",cond2)
+if cond2 > 0:
+    print("Condition vérifiée")
+else:
+    print("Condition NON vérifiée")
+
+print("Valeur de la condition 3 :",cond3)
+if cond3 > 0:
+    print("Condition vérifiée")
+else:
+    print("Condition NON vérifiée")
 
 # Variante : avec numpy pour obtenir l'expression des valeurs propres de la matrice :
 J_star = np.array([[a1, a2], [a3, a4]])
+print("Liste des valeurs propres de la matrice linéarisée tangente \n au point critique (obtenu avec numpy.linalg.eigvals) :")
 print(np.linalg.eigvals(J_star))
